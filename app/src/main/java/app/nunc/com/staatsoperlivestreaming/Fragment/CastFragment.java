af59aad87843f8674cbab92c875ba58d12736c48
przemyslawInwedo <przemyslaw.taborowski@inwedo.com>
2019-01-28 14:34:03 +0100

    cast list is now looking fine

--- app/src/main/java/app/nunc/com/staatsoperlivestreaming/Fragment/CastFragment.java
+++ app/src/main/java/app/nunc/com/staatsoperlivestreaming/Fragment/CastFragment.java
@@ -3,13 +3,13 @@ package app.nunc.com.staatsoperlivestreaming.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Callback;
@@ -21,7 +21,8 @@ public class CastFragment extends Fragment {
 
     private int position;
     private ImageView coverPhoto;
-    private TabLayout tabLayout;
+    private LinearLayout llCast;
+    private boolean isFullCastAdded0, isFullCastAdded1, isFullCastAdded2;
 
     public CastFragment() {
         // Required empty public constructor
@@ -31,11 +32,12 @@ public class CastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_description, container, false);
+        View view = inflater.inflate(R.layout.fragment_cast, container, false);
 
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        int position = sharedPref.getInt("position", 0);
+        position = sharedPref.getInt("position", 0);
+
+        llCast = view.findViewById(R.id.ll_tips);
 
         coverPhoto = view.findViewById(R.id.cover_photo);
         String photoUrl = LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getImg();
@@ -57,31 +59,59 @@ public class CastFragment extends Fragment {
         TextView tvTitle = view.findViewById(R.id.tv_title);
         tvTitle.setText("Cast");
 
-        String fullCast = "";
-
         for (int i = 0; i < LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().size(); i++) {
-            fullCast = fullCast.concat(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().get(i).toString());
-            if (i == LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().size()-1) {
-                fullCast = fullCast.concat("\n");
-                fullCast = fullCast.concat("\n");
+
+            if (!isFullCastAdded0) {
+                addFullCast(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().get(i).getName(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().get(i).getRole(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getRole_type());
+                isFullCastAdded0 = true;
+            } else {
+                addCast(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().get(i).getName(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(0).getCasting().get(i).getRole());
             }
+
         }
         for (int j = 0; j < LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().size(); j++) {
-            fullCast = fullCast.concat(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().get(j).toString());
-            if (j == LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().size()-1) {
-                fullCast = fullCast.concat("\n");
-                fullCast = fullCast.concat("\n");
+            if (!isFullCastAdded1) {
+                addFullCast(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().get(j).getName(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().get(j).getRole(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getRole_type());
+                isFullCastAdded1 = true;
+            } else {
+                addCast(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().get(j).getName(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(1).getCasting().get(j).getRole());
             }
         }
 
         for (int k = 0; k < LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getCasting().size(); k++) {
-            fullCast = fullCast.concat(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getCasting().get(k).toString());
+            if (!isFullCastAdded2) {
+                addFullCast(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getCasting().get(k).getName(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getCasting().get(k).getRole(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getRole_type());
+                isFullCastAdded2 = true;
+            } else {
+                addCast(LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getCasting().get(k).getName(), LiveFragment.events.get(0).getResults().get(position).getMetaDataList().getCastList().get(2).getCasting().get(k).getRole());
+            }
         }
 
-        TextView tvDesc = view.findViewById(R.id.tv_desc);
-        tvDesc.setText(fullCast);
-
         return view;
+    }
+
+    private void addCast(String name, String value) {
+
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.cast_view, null);
+        TextView tvName = view.findViewById(R.id.tv_name);
+        tvName.setText(name);
+        TextView tvValue = view.findViewById(R.id.tv_value);
+        tvValue.setText(value);
+        llCast.addView(view);
+
+    }
+
+    private void addFullCast(String name, String value, String title) {
+
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.full_cast_view, null);
+        TextView tvTitle = view.findViewById(R.id.tv_title);
+        tvTitle.setText(title);
+        TextView tvName = view.findViewById(R.id.tv_name);
+        tvName.setText(name);
+        TextView tvValue = view.findViewById(R.id.tv_value);
+        tvValue.setText(value);
+        llCast.addView(view);
+
     }
 
 }
